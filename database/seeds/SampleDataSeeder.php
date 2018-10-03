9d8694ab16be048b2509b3b6722365a7fa53762d
Scott Crowley <scott@scottcrowley.com>
2018-10-03 11:25:18 -0700

    Fix Data Seeder

--- database/seeds/SampleDataSeeder.php
+++ database/seeds/SampleDataSeeder.php
@@ -19,7 +19,9 @@ class SampleDataSeeder extends Seeder
     public function run()
     {
         Schema::disableForeignKeyConstraints();
+
         $this->channels()->content();
+
         Schema::enableForeignKeyConstraints();
     }
 
@@ -31,6 +33,8 @@ class SampleDataSeeder extends Seeder
         Channel::truncate();
 
         factory(Channel::class, 10)->create();
+
+        return $this;
     }
 
     /**
