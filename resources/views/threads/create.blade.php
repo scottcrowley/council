9685d7e50a117aed7c060c8f1d093f659c1f510f
Scott Crowley <scott@scottcrowley.com>
2018-10-03 07:48:08 -0700

    Use services config instead I forgot that we originally put the recaptcha keys in config/services.php.

--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -32,7 +32,7 @@
                             <wysiwyg name="body" value="{{ old('body') }}"></wysiwyg>
                         </div>
                         <div class="form-group">
-                            <div class="g-recaptcha" data-sitekey="{{ config('council.recaptcha.key') }}"></div>
+                            <div class="g-recaptcha" data-sitekey="{{ config('services.recaptcha.key') }}"></div>
                         </div>
                         <div class="form-group">
                             <button type="submit" class="btn btn-outline-primary">Publish</button>
