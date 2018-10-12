0966e5bd543729d69400d94eedff5dd9be5a8797
Scott Crowley <scott@scottcrowley.com>
2018-10-11 17:50:29 -0700

    Apply fixes from StyleCI (#5)

--- app/Http/Controllers/PinnedThreadsController.php
+++ app/Http/Controllers/PinnedThreadsController.php
@@ -4,25 +4,25 @@ namespace App\Http\Controllers;
 
 use App\Thread;
 
- class PinnedThreadsController extends Controller
- {
-     /**
-      * Pin the given thread.
-      *
-      * @param \App\Thread $thread
-      */
-     public function store(Thread $thread)
-     {
-         $thread->update(['pinned' => true]);
-     }
+class PinnedThreadsController extends Controller
+{
+    /**
+     * Pin the given thread.
+     *
+     * @param \App\Thread $thread
+     */
+    public function store(Thread $thread)
+    {
+        $thread->update(['pinned' => true]);
+    }
 
-     /**
+    /**
      * Un-Pin the given thread.
      *
      * @param \App\Thread $thread
      */
-     public function destroy(Thread $thread)
-     {
-         $thread->update(['pinned' => false]);
-     }
- }
+    public function destroy(Thread $thread)
+    {
+        $thread->update(['pinned' => false]);
+    }
+}
