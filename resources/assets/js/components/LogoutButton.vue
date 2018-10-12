a2c9d33b137c5eba4a8747256ae249170209c853
Scott Crowley <scott@scottcrowley.com>
2018-10-12 08:51:28 -0700

    Vueify Logout Link... because i think it is not necessary to use hiddenforms when we use Vue

---
+++ resources/assets/js/components/LogoutButton.vue
@@ -0,0 +1,21 @@
+<template>
+    <a :href="route" class="dropdown-item" @click.prevent="logout">
+        <slot/>
+    </a>
+</template>
+ <script>
+    export default {
+        props: {
+            route: {
+                default: "/logout"
+            }
+        },
+         methods: {
+            logout() {
+                axios.post(this.route).then(response => {
+                    window.location.href = response.request.responseURL;
+                });
+            }
+        }
+    };
+</script>
--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -64,15 +64,7 @@
 
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                             <a class="dropdown-item" href="{{ route('profiles', Auth::user()) }}">My Profile</a>
-                            <a class="dropdown-item" href="{{ route('logout') }}"
-                                onclick="event.preventDefault();
-                                                document.getElementById('logout-form').submit();">
-                                {{ __('Logout') }}
-                            </a>
-
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                @csrf
-                            </form>
+                            <logout-button route="{{ route('logout') }}">Logout</logout-button>
                         </div>
                     </li>
                 @endguest
